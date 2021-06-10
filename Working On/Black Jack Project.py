d6d95a8133a1406fd666d0071737e71b56abf1ca
CalMcCormick <cal.mccormick@gmail.com>
2021-06-10 21:15:38 +0100

    Update Black Jack Project.py

--- Working On/Black Jack Project.py
+++ Working On/Black Jack Project.py
@@ -1,28 +1,64 @@
 ## As part of the 100 days of code, I will be making a simple version of blacjack to play in the terminal
 
-
-
-# Step 4: return values - give player option to draw again - A can be 1 or 13
-# Step 5: decide when bust
-# Step 6: dealers turn
-# Step 7: game logic
-
-# Step 1: create deck
-suits = ["c", "d", "h", "s"]
+import random
+# Creating deck in a visual way
+suits = [" of Clubs", " of Diamonds", " of Hearts", " of Spades"]
 card_names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 cards = []
 for card in card_names:
     for suit in suits:
         cards.append(card+suit)
+
+# Assigning corresponding values to each card
 card_values = {}
 for card in cards:
     if card[0] == "J" or card[0] == "Q" or card[0] == "K" or card[0] == "A":
         card_values[card] = 10
     else:
         card_values[card] = int(card[0])
-print(card_values)
 
-import random
-# Step 2: deal cards
-def deal_cards():
-    pass
+# Creating a player class, so each function can be called easier and more efficiently
+class Player():
+
+    def __init__(self, name):
+        self.name = name
+
+    def deal_two_cards(self):
+
+        dealt_cards = []
+        dealt_count = 0
+
+        while dealt_count < 2:
+            random_num = random.randint(1, len(cards)+1)
+            random_card = cards.pop(random_num)
+            dealt_cards.append(random_card)
+            dealt_count += 1
+    
+        print("{}: {}, {}".format(self.name, dealt_cards[0], dealt_cards[1]))
+        return dealt_cards[0], dealt_cards[1]
+
+    def deal_one_card(self):
+
+        random_num = random.randint(1, len(cards)+1)
+        random_card = cards.pop(random_num)
+        print("{} pulled: {}".format(self.name, random_card))
+        return random_card
+
+    def hit(self, choice):
+
+        if choice[0].lower() == "h":
+            extra_card = self.deal_one_card()
+            return extra_card
+
+# Carry on in the morning good sir.
+
+class CardsInPlay():
+
+    def __init__(self, card_name):
+
+        self.holding = card_name
+
+    def card_value(self):
+
+        pass
+
