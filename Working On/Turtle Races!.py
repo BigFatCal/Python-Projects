027c314dc99287f15204a0b8e20f8eb8840fd364
CalMcCormick <cal.mccormick@gmail.com>
2021-06-19 20:57:43 +0100

    Update Turtle Races!.py

--- Working On/Turtle Races!.py
+++ Working On/Turtle Races!.py
@@ -1,5 +1,4 @@
-# This is a racing game made up with the names of me and my friend group
-# each colour is the colour each of us use in among us
+# This scripts allows you to add players, add their colours then race!
 
 import random
 from turtle import Screen, Turtle, color, pencolor, penup
@@ -14,48 +13,40 @@ screen.title("Why U Go So Fast")
 
 colours = []
 names = []
-num_players = int(screen.numinput("Player Count", "How many players??"))
-
-for x in range(num_players):
-    player = screen.textinput("Name", "Who is player "+str(x)+"?")
-    colour = screen.textinput("Colour", "What colour do you want to be, {}?".format(player))
-    colours.append(colour.lower)
-
-
-is_race_on = False
-
-user_bet = screen.textinput("Betty Betty Bet Bet", "Who you backing?")
-if user_bet not in names:
-    user_bet = screen.textinput("Pick a valid name!", "Who you got?:")
-
-y_coors = []
-seperation = (250/num_players)*2
-for x in range(num_players):
-    y_coors.append(125-seperation)
-
-
-for x in range(1,num_players+1):
-
-    fresh_turtle = Turtle()
-    fresh_turtle.speed(0)
-    fresh_turtle.pu()
-    fresh_turtle.hideturtle()
-    fresh_turtle.goto(-490, y_coors[x]-12)
-    fresh_turtle.write(names[x],font=("Arial",16))
+num_players = int(screen.numinput("Player Count", "How many players?? (max 6)"))
+y_coors = 125
 
 all_turtles = []
 for x in range(1,num_players+1):
 
+    # creating player turtle
+    player = screen.textinput("Name", "Who is player "+str(x)+"?")
+    names.append(player)
+    colour = screen.textinput("Colour", "What colour do you want to be, {}?".format(player))
     fresh_turtle = Turtle(shape="turtle")
     fresh_turtle.pu()
-    fresh_turtle.fillcolor(colours[x])
-    fresh_turtle.pencolor(colours[x])
-    fresh_turtle.goto(-400, y_coors[x])
+    fresh_turtle.fillcolor(colour.lower())
+    fresh_turtle.pencolor(colour.lower())
+    fresh_turtle.goto(-400, y_coors)
     all_turtles.append(fresh_turtle)
+    # creating turtle to write players name
+    writing_turtle = Turtle()
+    writing_turtle.hideturtle()
+    writing_turtle.pu()
+    writing_turtle.goto(-475, y_coors-8)
+    writing_turtle.pd()
+    writing_turtle.write("{}".format(player), font=("Arial",12))
+    
+    y_coors -= 50
 
-turtle_names = {}
-for x in range(1,num_players+1):
-    turtle_names[all_turtles[x]] = names[x]
+    
+
+
+turtle_names = {k:v for k,v in zip(all_turtles, names)}
+
+is_race_on = False
+
+user_bet = screen.textinput("Yeet", "Are you ready?")
 
 finish_line = Turtle()
 finish_line.hideturtle()
@@ -69,19 +60,9 @@ finish_line.right(90)
 finish_line.pd()
 finish_line.forward(280)
 
-your_bet = Turtle()
-your_bet.hideturtle()
-your_bet.pu()
-your_bet.goto(-20, 145)
-your_bet.pd()
-your_bet.write("Your bet: "+user_bet)
-
-
-
 if user_bet:
     is_race_on = True
     
-
 winner = 0
 while is_race_on:
 
@@ -93,28 +74,21 @@ while is_race_on:
 
             else:
                 turtle.pd()
-                turtle.forward(random.randint(0,15))
+                turtle.forward(random.randint(0,10))
+
 
 
 new_turtle = Turtle()
 new_turtle.hideturtle()
+new_turtle.pu()
+new_turtle.goto(-75,-15)
+new_turtle.pd()
+new_turtle.write("{} won!".format(turtle_names[winner]), font=("Arial",20))
 
-if user_bet == turtle_names[winner]:
-    
-    new_turtle.pu()
-    new_turtle.goto(-200,-15)
-    new_turtle.pd()
-    new_turtle.write("You win! 10 points to griffyndor!", font=("Arial",20))
-
-else:
-
-    new_turtle.pu()
-    new_turtle.goto(-200,-15)
-    new_turtle.pd()
-    new_turtle.write("HAHAHAHAHA YOU IDIOT YOU LOST", font=("Arial",20))
 
 
 
 screen.exitonclick()
 
-#### FIX SO YOU CAN HAVE ANY NUMBER OF PEOPLE, HAVE A RANDOM COLOUR AND WORK ACCORDINGLY
+#### UPDATE TO FIX SPACING, SPEED & CREATE A GIF POP UP AT THE END
+#### EXCEPTION AND ERROR HANDLING FOR COLOURS AND NUM OF PLAYERS
