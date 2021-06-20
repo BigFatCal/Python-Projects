23d13d002fa1b0b91fb0555ff62e57e2dd8a4b17
CalMcCormick <cal.mccormick@gmail.com>
2021-06-20 11:15:40 +0100

    Update Turtle Races!.py

--- Working On/Turtle Races!.py
+++ Working On/Turtle Races!.py
@@ -11,9 +11,13 @@ screen.setup(1000, 350)
 screen.title("Why U Go So Fast")
 
 
-colours = []
+colours = ["red", "blue", "green", "purple", "deeppink", "black"]
+random.shuffle(colours)
 names = []
 num_players = int(screen.numinput("Player Count", "How many players?? (max 6)"))
+if num_players > 6:
+    num_players = int(screen.numinput("Player Count", "How many players?? No more than 6!"))
+
 y_coors = 125
 
 all_turtles = []
@@ -22,11 +26,10 @@ for x in range(1,num_players+1):
     # creating player turtle
     player = screen.textinput("Name", "Who is player "+str(x)+"?")
     names.append(player)
-    colour = screen.textinput("Colour", "What colour do you want to be, {}?".format(player))
     fresh_turtle = Turtle(shape="turtle")
     fresh_turtle.pu()
-    fresh_turtle.fillcolor(colour.lower())
-    fresh_turtle.pencolor(colour.lower())
+    fresh_turtle.fillcolor(colours[x])
+    fresh_turtle.pencolor(colours[x])
     fresh_turtle.goto(-400, y_coors)
     all_turtles.append(fresh_turtle)
     # creating turtle to write players name
@@ -38,8 +41,6 @@ for x in range(1,num_players+1):
     writing_turtle.write("{}".format(player), font=("Arial",12))
     
     y_coors -= 50
-
-    
 
 
 turtle_names = {k:v for k,v in zip(all_turtles, names)}
@@ -74,16 +75,22 @@ while is_race_on:
 
             else:
                 turtle.pd()
-                turtle.forward(random.randint(0,10))
+                if num_players <= 2:
+                    turtle.forward(random.randint(0,5))
 
+                elif num_players <= 4:
+                    turtle.forward(random.randint(0,8))
 
+                else:
+                    turtle.forward(random.randint(0,11))
+                    
 
 new_turtle = Turtle()
 new_turtle.hideturtle()
 new_turtle.pu()
-new_turtle.goto(-75,-15)
+new_turtle.goto(-75,140)
 new_turtle.pd()
-new_turtle.write("{} won!".format(turtle_names[winner]), font=("Arial",20))
+new_turtle.write("{} won!".format(turtle_names[winner]), font=("Arial",16))
 
 
 
